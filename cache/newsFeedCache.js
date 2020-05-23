ce2499c5095d4f7b970720ba8db251bbdd38a15e
charan7386 <gummadavallicharansai12@gmail.com>
2020-05-23 19:02:20 +0530

    added 'getNewsFeed' function

--- cache/newsFeedCache.js
+++ cache/newsFeedCache.js
@@ -6,7 +6,10 @@ let feedCache = new Object();
 const maxDuration = 5*86400*1000; 
 const removeRedundancyInterval = 30*60*1000;
 
-
+/* 
+    user_uuid, post_uuid : unique ids
+    created_at : time stamp
+*/
 const addPostToCache = (user_uuid, post_uuid, created_at) => {
     if(user_uuid in feedCache){
         if(feedCache[user_uuid].length > 25) feedCache[user_uuid].shift();
@@ -33,8 +36,25 @@ const removeRedundancy = () => {
     }, removeRedundancyInterval);
 };
 
+/* 
+    user_uuids : list
+    last_fetch : time stamp
+*/
+const getNewsFeed = (user_uuids, last_fetch) => {
+    let posts = [];
+    for(let index = 0; index < user_uuids.length; index += 1){
+        if(!(user_uuids[index] in feedCache)) continue;
+        for(let post in feedCache[user_uuids[index]]){
+            if(post[1] > last_fetch) posts.push(posts);
+            else break;
+        }
+    }
+    return posts;
+}
+
 module.exports = {
     addPostToCache,
     removeRedundancy,
+    getNewsFeed,
     feedCache,
 }
